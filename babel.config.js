2a935b263d10d09f9755a6906a44ff1ef7efef37
Jerry Castro <jerry.castro@hp.com>
2021-12-28 18:47:51 -0300

    update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,8 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
+    presets: ['babel-preset-expo'],
     plugins: [
-      [
-        'module-resolver',
-        {
-          extensions: ['.tsx', '.ts', '.js', '.json'],
-        }
-      ],
       'react-native-reanimated/plugin' 
     ]
   };
