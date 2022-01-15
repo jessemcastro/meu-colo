4e2d6a92ecb39161a2ac08619c19809dcc954800
Jerry Macedo Castro <leom551@hotmail.com>
2022-01-15 19:04:08 -0300

    create simple animation and add props to card

--- src/components/CardWithButton/index.jsx
+++ src/components/CardWithButton/index.jsx
@@ -1,27 +1,35 @@
-import * as React from "react";
-import { Text, View, Image } from "react-native";
-import CommonButton from "../CommonButton";
-import styles from "./styles";
-import { COLORS } from "../../styles/colors";
+import * as React from 'react';
+import { Text, View, Image } from 'react-native';
+import CommonButton from '../CommonButton';
+import styles from './styles';
+import { COLORS } from '../../styles/colors';
 
-const CardWithButton = ({ introText }) => {
+const CardWithButton = ({ introText, completeText }) => {
+  const [isShowCompleteText, setIsShowCompleteText] = React.useState(false);
+
+  const handlePress = () => {
+    setIsShowCompleteText(!isShowCompleteText);
+  };
+
   return (
     <View style={styles.mainContainer}>
-      <Image
-        source={require("../../assets/images/cancer.png")}
-        style={{ width: "100%", height: 160 }}
-      />
-      <View
-        style={{
-          borderBottomWidth: 2,
-          borderBottomColor: COLORS.orange,
-          width: "30%",
-          borderRadius: 5,
-        }}
-      />
-      <Text>{introText}</Text>
-      <View style={{ width: "100%" }}>
-        <CommonButton buttonText={"SAIBA MAIS"} />
+      {!isShowCompleteText && (
+        <Image
+          source={require('../../assets/images/cancer.png')}
+          style={{ width: '100%', height: 160 }}
+        />
+      )}
+      {!isShowCompleteText && <View style={styles.separator} />}
+      <Text style={styles.subtitle}>{introText}</Text>
+      {isShowCompleteText && <View style={styles.separator} />}
+      {isShowCompleteText && (
+        <Text style={styles.completeText}>{completeText}</Text>
+      )}
+      <View style={{ width: '80%', paddingBottom: 10 }}>
+        <CommonButton
+          buttonText={!isShowCompleteText ? 'SAIBA MAIS' : 'VOLTAR'}
+          handlePress={handlePress}
+        />
       </View>
     </View>
   );
