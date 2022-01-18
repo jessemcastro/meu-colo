ff89fd2cb003bcf93d9af4ee79d2bbf5613dd423
Jerry Macedo Castro <jerry.castro96@outlook.com>
2022-01-17 22:56:16 -0300

    create animation for text card

--- src/components/CardWithButton/index.jsx
+++ src/components/CardWithButton/index.jsx
@@ -3,13 +3,36 @@ import { Text, View, Image } from 'react-native';
 import CommonButton from '../CommonButton';
 import styles from './styles';
 import { COLORS } from '../../styles/colors';
+import Animated, {
+  Easing,
+  FlipInEasyX,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
+import { useEffect } from 'react';
 
 const CardWithButton = ({ introText, completeText }) => {
   const [isShowCompleteText, setIsShowCompleteText] = React.useState(false);
+  const position = useSharedValue(300);
 
   const handlePress = () => {
     setIsShowCompleteText(!isShowCompleteText);
+    position.value = withTiming(0, {
+      duration: 2000,
+      easing: Easing.bounce,
+    });
   };
+
+  const positionStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: position.value,
+        },
+      ],
+    };
+  });
 
   return (
     <View style={styles.mainContainer}>
@@ -23,8 +46,11 @@ const CardWithButton = ({ introText, completeText }) => {
       <Text style={styles.subtitle}>{introText}</Text>
       {isShowCompleteText && <View style={styles.separator} />}
       {isShowCompleteText && (
-        <Text style={styles.completeText}>{completeText}</Text>
+        <Animated.Text style={[styles.completeText, positionStyle]}>
+          {completeText}
+        </Animated.Text>
       )}
+
       <View style={{ width: '80%', paddingBottom: 10 }}>
         <CommonButton
           buttonText={!isShowCompleteText ? 'SAIBA MAIS' : 'VOLTAR'}
