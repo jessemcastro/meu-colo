dd0469eb4b09733ba27433f812bb31a682e832f2
Jerry Macedo Castro <jerry.castro96@outlook.com>
2022-02-08 21:04:53 -0300

    fix styles

--- src/pages/pregnance/styles.js
+++ src/pages/pregnance/styles.js
@@ -2,9 +2,8 @@ import { StyleSheet } from 'react-native';
 import { COLORS } from '../../styles/colors';
 const styles = StyleSheet.create({
   defaultContainer: {
-    flex: 1,
     backgroundColor: COLORS.background,
-    padding: '12%',
+    paddingHorizontal: '8%',
   },
 });
 
